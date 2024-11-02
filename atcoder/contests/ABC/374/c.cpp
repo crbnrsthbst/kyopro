b2d9bf6d9ea5409ace75d0a8203cffdfe5cdc3f0
crbnrsthbst <shinichiro.saito6221@gmail.com>
2024-11-02 22:32:21 +0900

    worng commits

--- atcoder/contests/ABC/374/c.cpp
+++ atcoder/contests/ABC/374/c.cpp
@@ -4,29 +4,4 @@ using namespace std;
 #define rep1(i, n) for (int i = 1; i < (int)(n + 1); i++)
 #define rep2(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
 
-int main() {
-  int n;
-  cin >> n;
-  long long k[n];
-  long long totalEmp = 0;
-  rep(i, n) {
-    cin >> k[i];
-    totalEmp += k[i];
-  }
-
-  long long ans = 0;
-  long long minMax = 10000000000;
-  rep(group, (1 << n)) {
-    long long sumA = 0;
-    rep(i, n) {
-      if (group & (1 << i)) {
-        sumA += k[i];
-      }
-    }
-    long long m = max(sumA, totalEmp - sumA);
-    minMax = min(minMax, m);
-  }
-
-  cout << minMax << endl;
-  return 0;
-}
+int main() { return 0; }
