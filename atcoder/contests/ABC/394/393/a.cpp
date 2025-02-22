349a6bad0b7391760dc211d98db75a0298e7eb35
crbnrsthbst <shinichiro.saito6221@gmail.com>
2025-02-22 22:01:28 +0900

    not bad

---
+++ atcoder/contests/ABC/394/393/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n + 1); i++)
+#define rep2(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+
+int main() {
+  string s;
+  cin >> s;
+  rep(i, s.length()) {
+    if (s[i] == '2') {
+      cout << s[i];
+    }
+  }
+  cout << endl;
+  return 0;
+}
---
+++ atcoder/contests/ABC/394/393/d.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n + 1); i++)
+#define rep2(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+
+int main() {
+  string begins = "([<";
+  string ends = ")]>";
+  string s;
+  cin >> s;
+  if (s.length() == 1) {
+    cout << "No" << endl;
+    return 0;
+  }
+  stack<char> beginsWithoutEnd;
+  rep(i, s.length()) {
+    if (ends.find(s[i]) == string::npos) {
+      beginsWithoutEnd.push(s[i]);
+    } else {
+      if (beginsWithoutEnd.size() == 0) {
+        cout << "No" << endl;
+        return 0;
+      }
+      if (begins.find(beginsWithoutEnd.top()) != ends.find(s[i])) {
+        cout << "No" << endl;
+        return 0;
+      };
+      beginsWithoutEnd.pop();
+    }
+  }
+  if (beginsWithoutEnd.size() != 0) {
+    cout << "No" << endl;
+    return 0;
+  }
+  cout << "Yes" << endl;
+  return 0;
+}
