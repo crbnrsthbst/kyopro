7f83b88c08bea45cff864152a4a2f2efe45fbd54
crbnrsthbst <shinichiro.saito6221@gmail.com>
2025-03-15 14:53:00 +0900

    dfs

--- atcoder/problems/chosen100/dfs/26.cpp
+++ atcoder/problems/chosen100/dfs/26.cpp
@@ -5,41 +5,48 @@ using namespace std;
 #define rep2(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
 #define all(x) (x).begin(), (x).end()
 using Graph = vector<vector<int>>;
-const int dy[8] = {-1, 1, 0, 0, 1, 1, -1, -1};
-const int dx[8] = {0, 0, -1, 1, 1, -1, 1, -1};
-int w, h;
-vector<int> counter;
-void dfs(Graph &g, vector<bool> &done, int v, int parentCounter) {
-  done[v] = true;
-  counter[v] += parentCounter;
-  for (int nv : g[v]) {
-    if (done[nv]) continue;
-    dfs(g, done, nv, counter[v]);
+vector<int> cnt;
+vector<bool> visited;
+vector<int> rootInc;
+void dfs(int r, int cntParent, Graph &g) {
+  visited[r] = true;
+  cnt[r] = cntParent + rootInc[r];
+  for (int c : g[r]) {
+    if (visited[c]) {
+      continue;
+    }
+    dfs(c, cnt[r], g);
   }
 }
 
 int main() {
   int n, q;
   cin >> n >> q;
-
   Graph g(n, vector<int>());
   rep(i, n - 1) {
-    int v, u;
-    cin >> v >> u;
-    g[v - 1].push_back(u - 1);
-    g[u - 1].push_back(v - 1);
+    int u, v;
+    cin >> u >> v;
+    u--;
+    v--;
+    g[u].push_back(v);
+    g[v].push_back(u);
   }
-  counter.assign(n, 0);
+  vector<int> p(q), x(q);
+  rootInc.assign(n, 0);
   rep(i, q) {
-    int v, cnt;
-    cin >> v >> cnt;
-    counter[v - 1] += cnt;
+    cin >> p[i] >> x[i];
+    p[i]--;
+    rootInc[p[i]] += x[i];
   }
-  vector<bool> done(n, false);
-  done[0] = true;
-  dfs(g, done, 0, 0);
-  for (auto cnt : counter) {
-    cout << cnt << " ";
+  cnt.assign(n, 0);
+  visited.assign(n, false);
+
+  dfs(0, 0, g);
+
+  for (int v : cnt) {
+    cout << v << " ";
   }
+  cout << endl;
+
   return 0;
 }
