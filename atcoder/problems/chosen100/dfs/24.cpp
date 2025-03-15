f1ac7ea957f08f6a687f40f5d1b7ca0afc6c33bf
crbnrsthbst <shinichiro.saito6221@gmail.com>
2025-03-15 11:56:58 +0900

    dfs

--- atcoder/problems/chosen100/dfs/24.cpp
+++ atcoder/problems/chosen100/dfs/24.cpp
@@ -7,37 +7,43 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 
 using Graph = vector<vector<int>>;
-vector<int> visited;
 vector<int> done;
-int t = 0;
-
-void dfs(Graph &g, int v) {
-  t++;
-  visited[v] = t;
-  for (auto child : g[v]) {
-    if (visited[child] == 0) dfs(g, child);
+vector<int> visited;
+int ts = 0;
+void dfs(int u, Graph &g) {
+  visited[u] = ++ts;
+  for (auto v : g[u]) {
+    if (!visited[v]) {
+      dfs(v, g);
+    }
   }
-  t++;
-  done[v] = t;
+  done[u] = ++ts;
 }
 
 int main() {
   int n;
   cin >> n;
-  Graph g(n + 1, vector<int>());
-
+  Graph g(n, vector<int>());
   rep(i, n) {
     int u, k;
     cin >> u >> k;
+    u--;
     rep(j, k) {
-      int v2;
-      cin >> v2;
-      g[u].push_back(v2);
+      int v;
+      cin >> v;
+      v--;
+      g[i].push_back(v);
     }
   }
-  visited.assign(n + 1, 0);
-  done.assign(n + 1, 0);
-  dfs(g, 1);
-  rep1(i, n) { cout << i << " " << visited[i] << " " << done[i] << endl; }
+  done.assign(n, 0);
+  visited.assign(n, 0);
+
+  rep(i, n) {
+    if (visited[i]) {
+      continue;
+    }
+    dfs(i, g);
+  }
+  rep(i, n) { cout << i + 1 << " " << visited[i] << " " << done[i] << endl; }
   return 0;
 }
